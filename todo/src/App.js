b720e256fec6e0123c33af961fa7fbbc7a821ecd
wangui.njoka <wangui.njoka@gmail.com>
2019-07-11 12:37:51 +0300

    added state to dos in the main app component and passed down to todos component

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,11 +3,30 @@ import Todos from './components/Todos'
 import './App.css';
 
 class App extends Component {
- 
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'meet bob',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'meet jane',
+        completed: false
+      },
+      {
+        id:3,
+        title: 'get job',
+        completed: false
+      }
+    ]
+  }
   render(){
+  
   return (
     <div className="App">
-    <Todos />
+    <Todos todos={this.state.todos}/>
     </div>
   );
 }
