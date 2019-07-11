f5d7aa08700b3a56d83fef73a81e0f551ae7eec6
wangui.njoka <wangui.njoka@gmail.com>
2019-07-11 14:02:13 +0300

    added todoitems component passed data from todos

--- todo/src/components/Todos.js
+++ todo/src/components/Todos.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react';
+import Todoitem from './Todoitem';
+
 
 class Todos extends Component {
   render(){
     
   return this.props.todos.map((todo) => (
-    <h3> { todo.title}</h3>)
-  );
+    <Todoitem key = {todo.id} todo={todo}/>
+  )
+);
 }
 }
 
