4685c239c8167dd2d236201ec600e7b739f3bab3
wangui.njoka <wangui.njoka@gmail.com>
2019-07-12 10:27:43 +0300

    added interactive styling

--- todo/src/components/Todoitem.js
+++ todo/src/components/Todoitem.js
@@ -2,9 +2,18 @@ import React,{Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Todoitem extends Component    {
+    getStyle = ()=> {
+        return {
+            background: '#4B9BB9',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+            'line-through': 'none'
+        }
+    }
     render(){
         return(
-           <div>
+           <div style = {this.getStyle()}>
                <p> { this.props.todo.title}</p>
     
            </div> 
