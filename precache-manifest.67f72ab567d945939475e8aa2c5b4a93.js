7fa969bd9cdcd5e9beaf7b77325557e83e9ffbec
wangui.njoka <wangui.njoka@gmail.com>
2019-07-15 23:38:36 +0300

    Updates

---
+++ precache-manifest.67f72ab567d945939475e8aa2c5b4a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023985865fc7f2cfac9d23a7b4a1a139",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "4ce0ef4ed53ae42fc391",
+    "url": "/todo/static/css/main.c55952e3.chunk.css"
+  },
+  {
+    "revision": "5acfd800d23e924fe964",
+    "url": "/todo/static/js/2.7fa37ec6.chunk.js"
+  },
+  {
+    "revision": "4ce0ef4ed53ae42fc391",
+    "url": "/todo/static/js/main.eac105ab.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
